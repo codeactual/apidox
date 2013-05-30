3cbe4431ae52b61e06bb32e4c3fb5f9adb537f25
David Smith <codeactual@gmail.com>
2013-05-30 13:57:20 -0700

    chore(grunt): Migrate to grunt-horde

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,78 +1,11 @@
 module.exports = function(grunt) {
   'use strict';
 
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-shell');
-
-  var mochaShelljsOpt = {stdout: true, stderr: false};
-
-  grunt.initConfig({
-    jshint: {
-      src: {
-        files: {
-          src: ['index.js', 'lib/**/*.js']
-        }
-      },
-      bin: {
-        files: {
-          src: ['bin/*']
-        }
-      },
-      grunt: {
-        files: {
-          src: ['Gruntfile.js']
-        }
-      },
-      tests: {
-        options: {
-          expr: true
-        },
-        files: {
-          src: ['test/lib/**/*.js']
-        }
-      },
-      json: {
-        files: {
-          src: ['*.json']
-        }
-      }
-    },
-    uglify: {
-      dist: {
-        options: {
-          compress: false,
-          mangle: false,
-          beautify: true
-        },
-        files: {
-          'dist/apidox.js': 'dist/apidox.js'
-        }
-      }
-    },
-    shell: {
-      options: {
-        failOnError: true
-      },
-      build: {
-        command: 'component install --dev && component build --standalone apidox --name apidox --out dist --dev'
-      },
-      dist: {
-        command: 'component build --standalone apidox --name apidox --out dist'
-      },
-      test_lib: {
-        options: mochaShelljsOpt,
-        command: 'mocha --colors --recursive --reporter spec test/lib'
-      },
-      dox_lib: {
-        command: 'bin/apidox --input lib/apidox/index.js --output docs/ApiDox.md'
-      }
-    }
-  });
-
-  grunt.registerTask('default', ['jshint']);
-  grunt.registerTask('dox', ['shell:dox_lib']);
-  grunt.registerTask('build', ['default', 'shell:build']);
-  grunt.registerTask('dist', ['default', 'shell:dist', 'uglify:dist', 'dox']);
-  grunt.registerTask('test', ['build', 'shell:test_lib']);
+  require('grunt-horde')
+    .create(grunt)
+    .demand('projName', 'apidox')
+    .demand('instanceName', 'apidox')
+    .demand('klassName', 'ApiDox')
+    .loot('node-component-grunt')
+    .attack();
 };
